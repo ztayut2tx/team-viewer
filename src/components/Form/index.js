7ceaf9e3b8f65eef7e3f980982ea06733054c7cd
Zach Taylor <ztayut2tx@gmail.com>
2021-04-01 16:29:20 -0500

    created search form that will populate employeeSearch component

---
+++ src/components/Form/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+    return (
+        <input
+        className="form-group search-box"
+        type="text"
+        placeholder="Find Employee"
+        ></input>
+    );
+}
+
+export default Form;
