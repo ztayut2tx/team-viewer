c4af049dbbe050bcf951aab4978e95eacdef50d4
Zach Taylor <ztayut2tx@gmail.com>
2021-04-01 16:36:48 -0500

    set state

--- src/components/EmployeeSearch/index.js
+++ src/components/EmployeeSearch/index.js
@@ -2,3 +2,12 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import Form from "../Form";
 
+class teamSearch extends Component {
+    state = {
+        search: "",
+        results: [],
+        endResults: [],
+    };
+
+    
+}
